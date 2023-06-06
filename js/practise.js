5beb58fa2f2e3e589006feb42bb4340d16c5d631
unknown <tom1@gmail.com>
2023-06-06 15:53:08 +0200

    message

---
+++ js/practise.js
@@ -0,0 +1,30 @@
+$(function() {
+    $("#btn_carousel").click(function() {
+        window.open("https://mldxtj1980.github.io/practise_carousel/")
+    })
+    $("#btn_map").click(function() {
+        window.open("https://mldxtj1980.github.io/practise_map/")
+    })
+    $("#btn_todo").click(function() {
+        window.open("https://mldxtj1980.github.io/practise_ToDoList/")
+    })
+    $("#btn_e").click(function() {
+        window.open("https://mldxtj1980.github.io/E-Learning/")
+    })
+
+    $("#btn_accordion").click(function() {
+        window.open("https://mldxtj1980.github.io/practise_accordion/")
+    })
+    $("#btn_angel").click(function() {
+        window.open("https://mldxtj1980.github.io/practise_flying_angel/")
+    })
+    $("#btn_light_picture").click(function() {
+        window.open("https://mldxtj1980.github.io/practise_highlight_picture/")
+    })
+    $("#btn_postblog").click(function() {
+        window.open("https://mldxtj1980.github.io/practise_postblog/")
+    })
+    $("#btn_tablis").click(function() {
+        window.open("https://mldxtj1980.github.io/practise_tablist/")
+    })
+})
---
+++ js/testautomation.js
@@ -0,0 +1,24 @@
+$(function () {
+  $("#webdriver").click(function () {
+    window.open("https://github.com/MLDXTJ1980/WebdriverProject")
+  })
+  $("#testng").click(function () {
+    window.open("https://github.com/MLDXTJ1980/TestNGBasic")
+  })
+  $("#restassured").click(function () {
+    window.open("https://github.com/MLDXTJ1980/RESTAssuredTest")
+  })
+  $("#mavenjava").click(function () {
+    window.open("https://github.com/MLDXTJ1980/Mavenjava")
+  })
+
+  $("#e2e").click(function () {
+    window.open("https://github.com/MLDXTJ1980/E2EProject")
+  })
+  $("#cucumber").click(function () {
+    window.open("https://github.com/MLDXTJ1980/Automation")
+  })
+  $("#apitesting").click(function () {
+    window.open("https://github.com/MLDXTJ1980/APIFramework")
+  })
+})
